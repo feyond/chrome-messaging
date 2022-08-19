efe468306b6007a7db02712835fb8e2fa13c05cb
chenfengyang <c.fy@qq.com>
2022-08-19 21:25:54 +0800

    jest 单元测试：background

--- __tests__/chrome-messaging-background.test.ts
+++ __tests__/chrome-messaging-background.test.ts
@@ -1,151 +1,83 @@
 import { chrome } from "jest-chrome";
-import { setDefaultLevel } from "@feyond/console-logging";
-import { onBackgroundMessage } from "@background";
-import { ChromeLocation, ChromeResponse } from "@core";
-
-declare global {
-	interface BackgroundTargetRoutes {
-		iTest: ITest;
-	}
-}
-
-interface ITest {
-	add(x: number, y: number): number;
-}
+import { background, onBackgroundMessage } from "@background";
+import { BackgroundTarget, ChromeLocation, ChromeRequest, ChromeResponse, ContentTarget } from "@core";
+import { IBGTest } from "../jest.setup";
 
 beforeAll(() => {
-	setDefaultLevel("debug");
 	onBackgroundMessage({
-		iTest: () =>
-			new (class implements ITest {
-				add(x: number, y: number): number {
-					return x + y;
-				}
-			})(),
+		iTest: new (class implements IBGTest {
+			hello(): string {
+				throw new Error("test route throw error");
+			}
+
+			add(x: number, y: number): number {
+				return x + y;
+			}
+		})(),
 	});
 });
 
-describe("@feyond/chrome-messaging-background", () => {
-	test("chrome message mismatch", () => {
-		const sendResponseSpy = jest.fn();
-		chrome.runtime.onMessage.callListeners({ greeting: "hello" }, {}, sendResponseSpy);
-		expect(sendResponseSpy).not.toBeCalled();
+describe("background", () => {
+	test("on background:listener message", () => {
+		const message: ChromeRequest<BackgroundTarget> = {
+			target: "iTest",
+			payload: {
+				fn: "hello",
+				args: [],
+			},
+			from: ChromeLocation.Popup,
+		};
+		chrome.runtime.onMessage.callListeners(
+			message,
+			{},
+			jest.fn((response) => {
+				expect(response).toEqual({ status: false, message: "test route throw error" });
+			})
+		);
+	});
+	test("on background:router call message", () => {
+		const message: ChromeRequest<BackgroundTarget> = {
+			target: "iTest",
+			payload: {
+				fn: "add",
+				args: [1, 2],
+			},
+			from: ChromeLocation.Popup,
+		};
+		background.callMessage(message, (response) => {
+			expect(response).toEqual({ status: true, data: 3 });
+		});
 	});
 
-	describe("chrome message match", () => {
-		beforeEach(() => {
-			Reflect.set(chrome.devtools.inspectedWindow, "tabId", 1);
-		});
-		afterEach(() => {
-			Reflect.deleteProperty(chrome.devtools.inspectedWindow, "tabId");
-		});
-		test("Could not retrieve target", (done) => {
-			const sendResponse = (response: ChromeResponse) => {
-				expect(response).not.toBeNull();
-				expect(response).toMatchObject({
-					status: false,
-					message: "Could not retrieve target route: hello.",
-				});
-				done();
-			};
-			chrome.runtime.onMessage.callListeners(
-				{ payload: { fn: "add", args: [1, 2] }, target: "hello", from: ChromeLocation.Devtools, tabId: 1 },
-				{},
-				sendResponse
-			);
-		});
-		test("Message payload:fn no specified", (done) => {
-			const sendResponse = (response: ChromeResponse) => {
-				expect(response).toMatchObject({
-					status: false,
-					message: "Message payload:fn no specified.",
-				});
-				done();
-			};
-			chrome.runtime.onMessage.callListeners(
-				{
-					payload: { args: [1, 2] },
-					target: "iTest",
-					from: ChromeLocation.Devtools,
-					tabId: 1,
-				},
-				{},
-				sendResponse
-			);
-		});
-		test("Message payload:args illegal.", (done) => {
-			const sendResponse = (response: ChromeResponse) => {
-				expect(response).toMatchObject({
-					status: false,
-					message: "Message payload:args illegal.",
-				});
-				done();
-			};
-			chrome.runtime.onMessage.callListeners(
-				{
-					payload: { fn: "add" },
-					target: "iTest",
-					from: ChromeLocation.Devtools,
-					tabId: 1,
-				},
-				{},
-				sendResponse
-			);
-		});
-		test("Method not exists.", (done) => {
-			const fn = "minus",
-				target = "iTest";
-			const sendResponse = (response: ChromeResponse) => {
-				expect(response).toMatchObject({
-					status: false,
-					message: `Method[${fn}] does not exist on target:${target}`,
-				});
-				done();
-			};
-			chrome.runtime.onMessage.callListeners(
-				{
-					payload: { fn, args: [1, 2] },
-					target,
-					from: ChromeLocation.Devtools,
-					tabId: 1,
-				},
-				{},
-				sendResponse
-			);
-		});
+	test("BGContentRoute", (done) => {
+		const toContent: ChromeRequest<ContentTarget> = {
+			target: "document",
+			payload: {
+				fn: "test",
+				args: [],
+			},
+			from: ChromeLocation.Popup,
+		};
+		const message: ChromeRequest<BackgroundTarget> = {
+			target: "content",
+			payload: {
+				fn: "forward",
+				args: [123, toContent],
+			},
+			from: ChromeLocation.Popup,
+		};
 
-		// test("Method payload:args type illegal.", (done) => {
-		// 	const fn = "add",
-		// 		target = "iTest";
-		// 	const sendResponse = (response: ChromeResponse) => {
-		// 		expect(response).toMatchObject({
-		// 			status: false,
-		// 			message: expect.any(String),
-		// 		});
-		// 		done();
-		// 	};
-		// 	chrome.runtime.onMessage.callListeners({ payload: { fn, args: [1] }, target, from: ChromeLocation.Devtools, tabId: 1 }, {}, sendResponse);
-		// });
-		test("message response success.", (done) => {
-			const fn = "add",
-				target = "iTest";
-			const sendResponse = (response: ChromeResponse) => {
-				expect(response).toMatchObject({
-					status: true,
-					data: 3,
-				});
-				done();
-			};
-			chrome.runtime.onMessage.callListeners(
-				{
-					payload: { fn, args: [1, 2] },
-					target,
-					from: ChromeLocation.Devtools,
-					tabId: 1,
-				},
-				{},
-				sendResponse
-			);
+		const callback = jest.fn((response) => {
+			expect(response).toEqual({ status: true, data: { status: true, data: "DOM Test" } });
+			done();
 		});
+		const sendContent = (tabId: number, message: ChromeRequest<ContentTarget>, responseCallback: (response: ChromeResponse) => void) => {
+			expect(tabId).toEqual(123);
+			expect(message).toEqual(toContent);
+			responseCallback({ status: true, data: "DOM Test" });
+		};
+		chrome.tabs.sendMessage.mockImplementation(sendContent as typeof chrome.tabs.sendMessage);
+
+		chrome.runtime.onMessage.callListeners(message, {}, callback);
 	});
 });
--- jest.setup.ts
+++ jest.setup.ts
@@ -3,3 +3,13 @@ import { setDefaultLevel } from "@feyond/console-logging/levels";
 
 Object.assign(global, { chrome });
 setDefaultLevel("debug");
+
+declare global {
+	interface BackgroundTargetRoutes {
+		iTest: IBGTest;
+	}
+}
+
+export interface IBGTest {
+	add(x: number, y: number): number;
+}
